0868f2edec4b348ffa33fcaa3bb64a1e3fc30ba3
Duran Audrey <audrey.duran@insa-lyon.fr>
2016-04-04 19:38:09 +0200

    Ajout plot criteres (max + moy)

--- Graph.py
+++ Graph.py
@@ -31,23 +31,53 @@ class PopGA:
 		self.Nb_Generation = Nb_Generation
 		self.T_Fit = T_Fit
 		self.pop = [Graph(Nb_node,random.choice(P_link),0,1) for i in range(Size)]
-		self.Fit = [0 for i in range(Size)]
+		self.Fit = [0. for i in range(Size)]
 		self.moyFit = []
 		self.minFit = []
 		self.maxFit = []
+
+		self.coeff_sw = [] #evolution de valeur du coeff sw pour le meilleur graphe a chaque generation
+		self.moyEnVueSw = 0 #stockee pour calcul moyenne sur les graphes a une gene donnee
+		self.moy_sw = [] #moyenne pour les graphes sur ttes les gene
+
+		self.coeff_c = [] #idem pour pc
+		self.moyEnVueC = 0
+		self.moy_c = []
+
+		self.coeff_d = [] #idem pour pd
+		self.moyEnVueD = 0
+		self.moy_d = []
+
+		# self.moy_d = []
     
 	def Run(self):
 		stop_bool= True
 		i = 0
-		self.pop[0].display("Tree_Initial_psw=%f_pd=%f_tc=%f.png"%(self.P_SW,self.P_D,self.Tc))
+		# self.pop[0].display("Tree_Initial_psw=%f_pd=%f_tc=%f.png"%(self.P_SW,self.P_D,self.Tc))
 		while stop_bool == True:
-			self.fitness()
+			indice_best=self.fitness()
+
 			#print "\nFitness of each individual = \n",self.Fit
-			moy = (sum(self.Fit)/len(self.Fit))
+
+			#FITNESS stockage
+			moy = (sum(self.Fit))/(len(self.Fit))
 			self.moyFit.append(moy)
 			#print "Moyenne : %f\n"%moy
 			self.minFit.append(min(self.Fit))
 			self.maxFit.append(max(self.Fit))
+
+			#COEFFS stockage
+			self.coeff_sw.append((self.pop[indice_best]).sw)
+			self.coeff_c.append((self.pop[indice_best]).clique)
+			self.coeff_d.append((self.pop[indice_best]).degre)
+
+			self.moy_sw.append(self.moyEnVueSw)
+			self.moy_c.append(self.moyEnVueC)
+			self.moy_d.append(self.moyEnVueD)
+
+
+			#SUITE RUN
+
 			self.selection()
 			self.mutation()
 			self.crossing_over()
@@ -57,7 +87,7 @@ class PopGA:
 		# Recuperer l'indice du meilleur graphe
 		Indice_maxFit = self.Fit.index(self.maxFit[-1])
 		# Display du meilleur graphe
-		self.pop[Indice_maxFit].display("Tree_Final_psw=%f_pd=%f_tc=%f.png"%(self.P_SW,self.P_D,self.Tc))
+		# self.pop[Indice_maxFit].display("Tree_Final_psw=%f_pd=%f_tc=%f.png"%(self.P_SW,self.P_D,self.Tc))
 		self.export(i)
 
 
@@ -70,24 +100,63 @@ class PopGA:
 		plt.legend()
 		plt.xlabel("nb generation")
 		plt.ylabel("fitness")
-		plt.savefig("test_psw=%f_pd=%f_tc=%f_TM10.png"%(self.P_SW,self.P_D,self.Tc))
+		plt.savefig("psw=%f_pd=%f_tc=%f_tm=%f_Fitness.png"%(self.P_SW,self.P_D,self.Tc,self.Tm))
 		plt.title("100 nodes, 80 individus")
 		#plt.show()
 
+		plt.figure()
+		plt.plot(xrange(i),self.coeff_sw,'b',label='best sw')
+		plt.plot(xrange(i),self.moy_sw,'b--',label='moy sw')
+
+		plt.plot(xrange(i),self.coeff_c,'r',label='best clique')
+		plt.plot(xrange(i),self.moy_c,'r--',label='moy clique')
+
+		plt.plot(xrange(i),self.coeff_d,'g',label='best degree')
+		plt.plot(xrange(i),self.moy_d,'g--',label='moy degree')
 
 
+
+		plt.xlabel=("nb generation")
+		plt.legend()
+		plt.savefig("psw=%f_pd=%f_tc=%f_tm=%f_Criteres.png"%(self.P_SW,self.P_D,self.Tc,self.Tm))
+
+
+
+	#maj les fitness
+	#renvoie l'indice de l'ind avec la meilleure fitness
 	def fitness(self):
 		# degL = []
 		# SWL = []
 		# cliqL = []
+		best_ind=-1 #initialisation de l'indice pour le meilleur individu
+		best_fitness=0 #initialisation de la meilleur fitness
+
+		self.moyEnVueSw=0
+		self.moyEnVueC=0
+		self.moyEnVueD=0
+
 		for i in range(len(self.pop)):
 			self.Fit[i] = self.pop[i].fit(self.P_C,self.P_D,self.P_SW)
+			self.moyEnVueSw+=self.pop[i].sw
+			self.moyEnVueC+=self.pop[i].clique
+			self.moyEnVueD+=self.pop[i].degre
+
+
+			if self.Fit[i]>best_fitness:
+				best_fitness=self.Fit[i]
+				best_ind=i
 			# val = self.pop[i].fit(self.P_C,self.P_D,self.P_SW)
 			# cliqL.append(val[0])
 			# degL.append(val[1])
 			# SWL.append(val[2])
-		print "Moy Clique : %g, Moy SW : %g, Moy deg : %g"%(sum(cliqL)/len(cliqL), sum(SWL)/len(SWL), sum(degL)/len(degL))
-		return 0
+		#print "Moy Clique : %g, Moy SW : %g, Moy deg : %g"%(sum(cliqL)/len(cliqL), sum(SWL)/len(SWL), sum(degL)/len(degL))
+		print "best",best_fitness
+		print "ind fit",i
+		self.moyEnVueSw/=self.Size
+		self.moyEnVueC/=self.Size
+		self.moyEnVueD/=self.Size
+
+		return best_ind
 
 
 	def mutation(self):
@@ -149,6 +218,10 @@ class Graph:
 		else :
 			self.graphe = unGraph.copy()
 		# Pour recuperer les nodes et edges faire self.graphe.nodes() et self.graphe.edges()
+
+		self.sw = 0 #carre de l'ecart entre la valeur observee et theorique
+		self.clique = 0 #valeur observee
+		self.degre = 0
 		
 
 	def display(self,name):
@@ -292,6 +365,8 @@ class Graph:
 			somme = 0.
 			edges = self.graphe.edges()
 			for i in self.graphe.nodes():
+
+				#CLIQUE
 				neighbors = self.graphe.neighbors(i)
 
 				k = len(neighbors)
@@ -312,7 +387,7 @@ class Graph:
 				somme = somme + (C -1/k)**2
 
 
-				
+				#SW
 				d=0.
 				for j in self.graphe.nodes():  #on regarde tous les autres noeuds
 					if i!=j:
@@ -323,10 +398,13 @@ class Graph:
 			
 			#Clique norme N
 			somme=float(somme/N)
+			self.clique=somme*2.5
 
 			# SW
 			dobs=float(dmoy/N) #distance moy sur tous les noeuds
-			e=exp(-((dobs-log(N))**2)/10)
+			coeff=(dobs-log(N))**2
+			e=exp(-coeff/10)
+			self.sw=coeff/10
 
 			# Degre
 			M = max(nb_neighbors)
@@ -336,8 +414,9 @@ class Graph:
 				Nk = nb_neighbors.count(k)
 				deg = deg + ( (float(Nk)/N) - (k**(-gamma)) )**2  # Est ce qu'on diviserait pas par M--
 			
+			self.degre=deg*0.9
 			#print "Clique : %g, SW : %g, deg : %g\n"%(somme*1.5, ((dobs-log(N))**2)/10, deg*0.9)
-			return (a*exp(-somme*2.5) + b*exp(-deg*0.9) + c*e)
+			return a*exp(-somme*2.5) + b*exp(-deg*0.9) + c*e
 			#return (somme*2.5, deg*0.9, ((dobs-log(N))**2)/10)
 		else :
 			for i in self.graphe.nodes() :
@@ -348,52 +427,56 @@ class Graph:
 			for k in xrange(1,M+1):
 				Nk = nb_neighbors.count(k)
 				deg = deg + ( (float(Nk)/N) - (k**(-gamma)) )**2
-			#return b*exp(-deg)
-			return (0,deg*0.9,0)
+			self.sw=0
+			self.clique=0
+			self.degre=deg*0.9
+
+			return b*exp(-deg*0.9)
+			#return (0,deg*0.9,0)
 
 
 
 # MAIN
 ## Parametres
 Nb_node = 100 #100 avant
-P_link = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] #Bien
-P_SW = 50./100
+P_link = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] #Bien 
+P_SW = 50./100 
 P_C = 25./100 #Parisot
 P_D = 25./100
-Size = 100
-Tm = 10/Size #Parisot
+Size = 30
+Tm = 1#Parisot
 Tc = 0.01
-Nb_Generation = 100 #50
+Nb_Generation = 20 #50
 T_Fit = 1 # valeur seuil de la fitness (critere d'arret)
 
 ## Creation de la population
 
-#pop1 = PopGA(Nb_node,P_link,P_SW,P_C,P_D,Size,Tm,Tc,Nb_Generation,T_Fit)
+pop1 = PopGA(Nb_node,P_link,P_SW,P_C,P_D,Size,Tm,Tc,Nb_Generation,T_Fit)
 
 
 print "----------------- RUN -----------------------"
-#pop1.Run()
+pop1.Run()
 
 #Test de plusieurs parametres
 
 tc1=[0.05, 0.15]
 
 
-k=1
-psw = 0.1
-for j in tc1:
-	print "k=",k
-	pd=1-P_C-psw
-	pop1 = PopGA(Nb_node,P_link,psw,P_C,pd,Size,Tm,j,Nb_Generation,T_Fit)
-	pop1.Run()
-	k=k+1
+# k=1
+# psw = 0.1
+# for j in tc1:
+# 	print "k=",k
+# 	pd=1-P_C-psw
+# 	pop1 = PopGA(Nb_node,P_link,psw,P_C,pd,Size,Tm,j,Nb_Generation,T_Fit)
+# 	pop1.Run()
+# 	k=k+1
 
 
-k=1
-psw = 0.01
-for j in tc1:
-	print "k=",k
-	pd=1-P_C-psw
-	pop1 = PopGA(Nb_node,P_link,psw,P_C,pd,Size,Tm,j,Nb_Generation,T_Fit)
-	pop1.Run()
-	k=k+1
+# k=1
+# psw = 0.01
+# for j in tc1:
+# 	print "k=",k
+# 	pd=1-P_C-psw
+# 	pop1 = PopGA(Nb_node,P_link,psw,P_C,pd,Size,Tm,j,Nb_Generation,T_Fit)
+# 	pop1.Run()
+# 	k=k+1
